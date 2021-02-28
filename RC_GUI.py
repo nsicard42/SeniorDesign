6b75432afc8b556d6542822d7e8380be1c88b5ee
David <dwils036@ucr.edu>
2021-02-27 21:38:01 -0800

    Updated Comments. Fixes need to be made before March 1, 2021

--- RC_GUI.py
+++ RC_GUI.py
@@ -1,73 +1,27 @@
 import time
-#import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import os
 
+node1="xxx"
+node2="xxx"
+node3="xxx"
+Msg1 = Label(text = "0") 
+Msg2 = Label(text = "0") 
+Msg3 = Label(text = "0") 
 
 
-#window = tk.Tk()
+
 file = open("myfile.log","r")
 
-#greeting = tk.Label(text = "hello world")
-#greeting.pack()
+
 root = Tk()
 root.title('RC_GUI')
-root.geometry("800x800")
+root.geometry("400x400")
 root.resizable(0,0)
 
-# create a main fram
-#main_frame =ttk.Frame(root)
-#main_frame.pack_propagate(0)
-#main_frame.pack(fill=BOTH, expand=1)
-
-# create a canvas
-#my_canvas = Canvas(main_frame)
-#my_canvas.pack_propagate(0)
-#my_canvas.pack (side=LEFT, fill=BOTH, expand=1)
-
-# add a scrollbar to the canvas
-#my_scrollbar = ttk.Scrollbar(main_frame, orient = VERTICAL, command=my_canvas.yview)
-#my_scrollbar.pack(side=RIGHT, fill=Y)
-
-
-# configure the canvas
-#my_canvas.configure(yscrollcommand=my_scrollbar.set)
-#my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion = my_canvas.bbox("all")))
-
-# create another frame in the canvas
-#second_frame = Frame(my_canvas)
-
-#add that frame to a window in canvas
-#my_canvas.create_window((0,0), window=second_frame, anchor="nw")
-
-#root.mainloop()
 while 1:
 	os.system("timeout 1s candump -L vcan0 > myfile.log")
-	
-
-	# create a main fram
-	#main_frame = Frame(root)
-	#main_frame.pack(fill=BOTH, expand=1)
-
-	# create a canvas
-	#my_canvas = Canvas(main_frame)
-	#my_canvas.pack (side=LEFT, fill=BOTH, expand=1)
-
-	# add a scrollbar to the canvas
-	#my_scrollbar = ttk.Scrollbar(main_frame, orient = VERTICAL, command=my_canvas.yview)
-	#my_scrollbar.pack(side=RIGHT, fill=Y)
-
-	# configure the canvas
-	#my_canvas.configure(yscrollcommand=my_scrollbar.set)
-	#my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion = my_canvas.bbox("all")))
-
-	# create another frame in the canvas
-	#second_frame = Frame(my_canvas)
-
-	#add that frame to a window in canvas
-	#my_canvas.create_window((0,0), window=second_frame, anchor="nw")
-
 
 	where = file.tell() #logs location where we are in file
 	line = str(file.readline()) #takes line and turns into a string
@@ -79,20 +33,36 @@ while 1:
                 file.seek(0) #where to go
 	else:
 		os.system("clear")
-		#if y[0]==0x80:
-			#y[0] = "Headlight"
-		#elif y[0]==0x01:
-			#y[0] = "Gas Gauge"
+		Msg1.destroy() #Allows the update of Message from node1
+		Msg2.destroy() #Allows the update of Message from node2
+		Msg3.destroy() #Allows the update of Message from node3
+
+		#if (y[0] == "080"): #Add in to test Node 1
+		node1 = y[2]
+		ID1 = Label(text = "Funny sensor1") #ID Node1 #FixME. ADD Proper Label Name
+		Msg1 = Label(text = y[2]) #Message from Node1
+		ID1.grid(row=1, column=1)
+		Msg1.grid(row=1, column=2)
+
+		#elif (y[0] == "010"): #Add in to test Node2
+		node2 = y[2]
+		ID2 = Label(text = "Funny sensor2") #ID Node2 #FixME. ADD Proper Label name
+		Msg2 = Label(text = y[2]) #Message from Node2
+		ID2.grid(row=2,column=1)
+		Msg2.grid(row=2,column=2)
 		#else:
-			#y[0] = "Speedometer"
-		print(y[0])
-		print(y[2])
+		node3 = y[2]
+		ID3 = Label(text = "Funny sensor3") #ID Node3 #FixME. ADD Proper label Name
+		Msg3 = Label(text = y[2]) #Message from Node3
+		ID3.grid(row=3,column=1)
+		Msg3.grid(row=3,column=2)
 
-	ID = Label(text = y[0]) #ID = tk.Label(text = y[0])
-	Msg = Label(text = y[2]) #ID = tk.Label(text = y[2])
-	ID.pack()
-	Msg.pack()
 
-	second_frame.update_idletasks() #root.update_idletasks()
-	second_frame.update() #root.update()
+		print("node 1: ", node1) #Terminal Print.Remove
+		print("node 2: ", node2) #Terminal Print.Remove
+		print("node 3: ", node3) #Terminal Print.Remove
+
+	root.update_idletasks() #Updates Window Information
+	root.update() #Packs Update
+
 
